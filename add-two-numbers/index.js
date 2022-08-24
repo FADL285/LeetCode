a5f6cf5c57f569a07465cfbb1104f43dc1b27aa6
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-08-24 11:56:17 +0200

    Solve Add Two Numbers problem 🚀✅

---
+++ add-two-numbers/index.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+const addTwoNumbers = function (l1, l2) {
+  const List = new ListNode(0);
+  let head = List;
+  let sum = 0;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || sum > 0) {
+    if (l1 !== null) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
+  }
+
+  return List.next;
+};
+
+/**
+ * Runtime: 154 ms
+ * Memory Usage: 46.7 MB
+ **/
